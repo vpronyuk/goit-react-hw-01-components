1c5e654d6c28c60f6570017659a884d30764d48f
Volodymyr Proniuk <111579555+vpronyuk@users.noreply.github.com>
2023-03-18 02:20:45 +0200

    Update App.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -4,14 +4,14 @@ import user from 'data/user.json';
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
+    // style={{
+    //   height: '100vh',
+    //   display: 'flex',
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   fontSize: 40,
+    //   color: '#010101',
+    // }}
     >
       <Profile user={user} />
     </div>
